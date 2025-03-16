4538c846eb90188718e8b788c8faf5091e627b68
Marcos Gabriel M <gmarcos38@gmail.com>
2025-03-16 14:17:50 -0300

    add get by id and put with id

--- src/app.js
+++ src/app.js
@@ -22,6 +22,19 @@ app.get('/books', (req, res) => {
     res.status(200).json(books)
 })
 
+app.get('/books/:id', (req, res) => {
+    let book = books.find((book) => book.id === Number(req.params.id))
+    res.status(202).json(book)
+})
+
+app.put('/books/:id', (req, res) => {
+    let book = books.find((book) => book.id === Number(req.params.id))
+    book.title = req.body.title
+
+    res.status(202).json(book)
+})
+
+
 app.post('/books', (req, res) => {
     books.push(req.body)
     res.status(201).send('Added book with success')
