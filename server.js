99a215025c84216fa9b84a1882553003366c543e
Marcos Gabriel M <gmarcos38@gmail.com>
2025-03-16 13:39:41 -0300

    add server routes and nodemon

--- server.js
+++ server.js
@@ -2,9 +2,14 @@ import http from "http"
 
 const PORT = 3000
 
+const routes = {
+    "/": "home",
+    "/about": "about",
+}
+
 const server = http.createServer((req, res) => {
     res.writeHead(200, {"Content-Type": "text/plain"})
-    res.end("Hello World")
+    res.end(routes[req.url])
 })
 
 server.listen(PORT, () => {
