6b3f1bade42de2ebdda584d8dc41c59a6bfb8062
Marcos Gabriel M <gmarcos38@gmail.com>
2025-03-16 14:02:29 -0300

    add post route to add a book on const

--- src/app.js
+++ src/app.js
@@ -1,9 +1,30 @@
 import express from "express"
 
 const app = express()
+app.use(express.json())
+
+const books = [
+    {
+        id: 1,
+        title: 'Lord of Rings'
+    },
+    {
+        id: 2,
+        title: 'The Hobbit'
+    }
+]
 
 app.get('/', (req, res) => {
     res.status(200).send('Welcome to Aurora!')
+})
+
+app.get('/books', (req, res) => {
+    res.status(200).json(books)
+})
+
+app.post('/books', (req, res) => {
+    books.push(req.body)
+    res.status(201).send('Added book with success')
 })
 
 export default app
