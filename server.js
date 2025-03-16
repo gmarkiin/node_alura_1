7e831722f8baaaf740fe8f31901f621e4ea44abf
Marcos Gabriel M <gmarcos38@gmail.com>
2025-03-16 13:47:49 -0300

    add express

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-import http from "http"
-
+// import http from "http"
+import app from './src/app.js'
 const PORT = 3000
 
-const routes = {
-    "/": "home",
-    "/about": "about",
-}
-
-const server = http.createServer((req, res) => {
-    res.writeHead(200, {"Content-Type": "text/plain"})
-    res.end(routes[req.url])
-})
-
-server.listen(PORT, () => {
+app.listen(PORT, () => {
     console.log("Server started on port 3000")
 })
